0c102cbcf33d24fc00cd24005f60b49b160a4a5c
PritamSnatak <pritam.pratiwar.snaatak@mygurukulam.co>
2025-01-16 11:13:21 +0530

    Create terraform_init.groovy

---
+++ src/org/terraform/terraform_init.groovy
@@ -0,0 +1,10 @@
+package org.terraform
+
+class terraform_init {
+    static void initTerraform(def steps, String dir = '.') { 
+        steps.sh """
+        cd ${dir} 
+        terraform init -reconfigure
+        """
+    }
+}
